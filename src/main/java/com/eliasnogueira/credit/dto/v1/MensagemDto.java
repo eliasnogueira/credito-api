89cf7ca5cbdbeac4058353dd245e23548eb01bcc
Elias Nogueira <elias.nogueira@gmail.com>
2021-01-17 21:17:25 +0100

    adicionado construtor implicito

--- src/main/java/com/eliasnogueira/credit/dto/v1/MensagemDto.java
+++ src/main/java/com/eliasnogueira/credit/dto/v1/MensagemDto.java
@@ -28,8 +28,10 @@ import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 @ApiModel(value = "Mensagem v1")
 public class MensagemDto {
